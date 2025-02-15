4836eee62a7f96e996a2ae95f6fef3ee328a696e
ikram <ikramelazhary78@gmail.com>
2025-02-15 23:40:09 +0100

    last not list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './navbar'; // Assurez-vous que le chemin est correct pour votre projet
+import TopNavbar from './TopNavbar';
+import PostAd from './PostAd';
+import CategoryPage from "./CategoryPage";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import ManageAds from './ManageAds';
+// Composants pour les pages
+import Home from './HomePage';
+import About from './About';
+import Login from './Login';
+import Dashboard from './dashboard';
 
-function App() {
+
+
+const App = () => {
+  // Gérer l'état du mode sombre dans le composant parent
+  const [darkMode, setDarkMode] = useState(false);
+
+  // Toggle pour changer le mode sombre
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <TopNavbar toggleDarkMode={toggleDarkMode} />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/postad" element={<PostAd />} />
+        <Route path="/category/:categoryName" element={<CategoryPage />} />
+        <Route path="/admin/manage-ads" element={<ManageAds />} />
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+       
+        
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom/client';  // Utilisation de 'react-dom/client' avec React 18+
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';  // Pour intégrer redux-persist avec React
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./i18n"; // Importer la configuration de i18n
+import store, { persistor } from './Store';  // Import du store Redux
 
+// Crée un "root" pour React 18
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Rendu de l'application
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
